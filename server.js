0ae6cfe55b60c30b2e94841e7936152a2d007109
Raghunath.Venkata <sv_raghunath@hotmail.com>
2020-10-26 14:39:25 +0000

    msg.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-  app.use(express.static(__dirname + '/dist/dwp'));
-  app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/dist/apps/bpdweb/index.html')));
+  app.use(express.static(__dirname + './dist/dwp'));
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname + './dist/apps/bpdweb/index.html')));
 
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
