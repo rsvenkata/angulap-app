25e05a7c577176228970d70395fa2febcd5f351a
Raghunath.Venkata <sv_raghunath@hotmail.com>
2020-10-26 14:30:58 +0000

    first commit

---
+++ apps/bpdweb/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dwp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bpdweb';
+}
